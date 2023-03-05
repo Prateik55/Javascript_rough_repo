4151ef3766465242b093fa871646e1a0e5836456
Prateik <itspsr5@gmail.com>
2023-03-05 20:44:24 +0530

    second attempt in question

--- DSA_Lectures/04_Multiple_Pointers.js
+++ DSA_Lectures/04_Multiple_Pointers.js
@@ -1,20 +1,15 @@
-
+let arr2=[];
 function sumZero(arr){
     arr.sort((a,b)=>a-b);
+    
     for (let i of arr){
-      for (let j of arr){
-            let arr2=[];  
+      for (let j of arr){         
             if ((i+j)===0){
               arr2.push(i,j);
-              arr[j]--
               return arr2;
-              }
-      else{
-        arr[j]--
-      }
-      console.log(arr2)
+              } 
      }
-    arr[i]++
+   
     }
 }
 console.log(arr2);
