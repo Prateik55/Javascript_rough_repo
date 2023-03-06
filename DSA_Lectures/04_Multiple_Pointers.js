efd895391a7f8604ea9ce7c3e0013d38a05e586f
Prateik <itspsr5@gmail.com>
2023-03-06 19:02:27 +0530

    refactored with multiple pointer solution

--- DSA_Lectures/04_Multiple_Pointers.js
+++ DSA_Lectures/04_Multiple_Pointers.js
@@ -1,3 +1,5 @@
+
+
 let arr2=[];
 function sumZero(arr){
     arr.sort((a,b)=>a-b);
@@ -6,10 +8,29 @@ function sumZero(arr){
       for (let j of arr){         
             if ((i+j)===0){
               arr2.push(i,j);
-              return arr2;
-              } 
+               return arr2;
+      }
      }
-   
     }
 }
 console.log(arr2);
+//  ======================refactored code==================
+
+function sumZeroMP(arr) {
+    let left = 0;
+    let right = arr.length - 1;
+  
+    while (left < right) {
+      const sum = arr[left] + arr[right];
+  
+      if (sum === 0) {
+        return [arr[left], arr[right]];
+      } else if (sum < 0) {
+        left++;
+      } else {
+        right--;
+      }
+    }
+  
+    return undefined;
+  }
