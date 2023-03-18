e5a5b0db16e8904028e95a525023b068f9226f8d
Prateik <itspsr5@gmail.com>
2023-03-18 13:54:55 +0530

    check Duplicated using Multiple pointer

--- DSA_Lectures/E01_Q2_Exercise_checkDuplicates.js
+++ DSA_Lectures/E01_Q2_Exercise_checkDuplicates.js
@@ -1,12 +1,39 @@
+//! Write a function called sameFrequency. Given two positive integers, find out if the two numbers have the same frequency of digits.
+
+
+// Using frequencyCounter
+
 const areThereDuplicates = function (){
     let frequencyCounter={};
+
     for (let i of arguments){
         frequencyCounter[i]=(frequencyCounter[i]||0)+1;
     }
-for (let key in frequencyCounter){
-  if (frequencyCounter[key] > 1){
-    return true;
-  }
+        for (let key in frequencyCounter){
+            if (frequencyCounter[key] > 1){
+             return true;
+            }
 }
 return false;
 } 
+
+// Using multiple pointers
+// sorting the array -- may require the conversion of object to array 
+// initialising the left and right variables 
+// iterating through the whole array to cheeck if the value of left is equal to the value of thee rght 
+
+
+function areThereDuplicatesMP(...args) {
+    // Two pointers
+    args.sort((a,b) => a > b);
+    let start = 0;
+    let next = 1;
+    while(next < args.length){
+      if(args[start] === args[next]){
+          return true
+      }
+      start++
+      next++
+    }
+    return false
+  }
